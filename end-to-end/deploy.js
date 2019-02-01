bfa5f480f07b038c71f688aa6e8f00b14e5ee87c
aion-kelvin <kelvin@aion.network>
2019-02-01 14:59:42 -0500

    fix deploy.js

--- end-to-end/deploy.js
+++ end-to-end/deploy.js
@@ -11,14 +11,13 @@ let endpoint = 'http://127.0.0.1:8545';
 let web3 = new Web3(new Web3.providers.HttpProvider(endpoint));
 
 
-let privateKey = process.argv[3]
-var jar = fs.readFileSync(process.argv[4]);
+let privateKey = process.argv[2]
+var jarFilename = process.argv[3]
 
 let acc = web3.eth.accounts.privateKeyToAccount(privateKey);
 console.log("Deploying", jarFilename)
 console.log("-- from account", acc.address);
 console.log("-- using endpoint", endpoint);
-
 
 var jar = fs.readFileSync(jarFilename);
 let contractEncoded = web3.avm.deployJar(new Uint8Array(jar)).initTypes().encodeToHex();
@@ -26,7 +25,6 @@ let contractEncoded = web3.avm.deployJar(new Uint8Array(jar)).initTypes().encode
 let ctAddress;
 let run = async () => {
     let tx = {gas: 4000000, gasPrice: 10000000000, data: contractEncoded, from: acc.address, type: '0xf'};
-    console.log("-- Transaction details:", tx);
     let signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
     let result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     console.log("-- Got receipt:",  result);
